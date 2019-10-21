d20d86ea917b81b4c662d517a15a032dea217eaf
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-21 11:47:08 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -16,6 +16,13 @@ def naming_system
               label: "Genus",
               sub_category: {
                 label: "Species",
-                sub_category: nil } } } } } } }
+                sub_category: nil 
+                } 
+              } 
+            } 
+          } 
+        } 
+      } 
+    }
 
 end
